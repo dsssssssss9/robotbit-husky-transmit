ec7d8fb0ec24bd08ca34c08bd5ddd2c4c0a83968
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-21 23:47:35 +0000

    basic radio send test

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        radio.sendNumber(1)
+    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        radio.sendNumber(2)
+    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        radio.sendNumber(3)
+    } else {
+        radio.sendNumber(0)
+    }
+})
+radio.setGroup(1)
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.OBJECTCLASSIFICATION)
 huskylens.clearOSD()
 huskylens.writeOSD("Remote Control", 92, 30)
 basic.showIcon(IconNames.StickFigure)
 basic.forever(function () {
-	
+    huskylens.request()
 })
